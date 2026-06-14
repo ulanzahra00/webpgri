5f09555ec2566639d5a468444585dfc9f71eee98
ulanzahra00 <msdcokro@gmail.com>
2026-06-14 13:03:06 +0800

    Add error reporting to admin/index.php for debugging

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,7 @@
 <?php
 // Router dashboard admin dan CRUD sederhana untuk semua modul utama.
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once __DIR__ . '/../includes/functions.php';
 require_once __DIR__ . '/../includes/admin_header.php';
 
